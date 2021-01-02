036440c03a715fbff8387d21de54a97965627567
m0ckinjay <m0ckinjay@yandex.com>
2021-01-03 00:31:11 +0300

    initial draft

--- Main.js
+++ Main.js
@@ -1,4 +1,9 @@
 function changebg(){
-  var bg=document.querySelector('.bg1');
+  var bg=document.getElementsByClassName('.b');
   bg.style='background:black';
- 
+}
+function display_the_date(){
+var display_date =  document.querySelector('.one');
+display_date.textContent = new Date().toDateString();
+display_date.style = "background:black;font-size:calc(1rem + 1vw);"
+}
