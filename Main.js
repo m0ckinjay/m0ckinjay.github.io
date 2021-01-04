1de869dd16b2aa1fc92eed5cde3ac410c94af5f3
m0ckinjay <m0ckinjay@yandex.com>
2021-01-05 00:51:28 +0300

    Added text-zoom

--- Main.js
+++ Main.js
@@ -3,4 +3,4 @@ function display_the_date(){
 var display_date =  document.querySelector('.section1');
 display_date.textContent = new Date().toDateString();
 display_date.style = "background:black;font-size:calc(1rem + 1vw);"
-}
+};
